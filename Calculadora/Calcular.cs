612078a27291da1a7dfcc16821832528804e348c
mayraAriana <alvarezmayra.ariana@gmail.com>
2017-04-07 23:05:39 -0300

    tp_1
    calculadora

---
+++ Calculadora/Calcular.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public class Calcular
+    {
+        /// <summary>
+        /// Metodo que realiza las operaciones aritmeticas.
+        /// </summary>
+        /// <param name="primernumero"></param>
+        /// <param name="segundonumero"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public double operar(Numero primernumero, Numero segundonumero, string operador)
+        {
+            double resultado = 0;
+
+            if(validarOperador(operador) == 1)
+            {
+                switch (operador)
+                {
+                    case "+":
+                        resultado = primernumero.numero + segundonumero.numero;
+                        break;
+                    case "-":
+                        resultado = primernumero.numero - segundonumero.numero;
+                        break;
+                    case "*":
+                        resultado = primernumero.numero * segundonumero.numero;
+                        break;
+                    case "/":
+                        if (segundonumero.numero != 0)
+                        {
+                            resultado = primernumero.numero / segundonumero.numero;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No es posible dividir por 0");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Coloque un operador previamente.");
+            }
+
+                return resultado;
+            }
+
+        /// <summary>
+        /// metodo de validacion
+        /// </summary>
+        /// <param name="operando"></param>
+        /// <returns></returns>
+        public double validarOperador(string operando)
+        {
+            double primernumero = 0;
+            if (operando == "+"|| operando =="*"||operando == "-"|| operando == "/")
+            {
+                primernumero = 1;
+            }
+            else
+            {
+                primernumero = 0;
+            }
+
+            return primernumero;
+        }
+
+    }
+}
---
+++ Calculadora/Numero.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public class Numero
+    {
+
+        public double numero;
+
+        /// <summary>
+        /// constructor sobrecargado
+        /// </summary>
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        /// <summary>
+        /// constructor sobrecargado
+        /// </summary>
+        /// <param name="primernumero"></param>
+        public Numero(string primernumero)
+        {
+            setNumero(primernumero);
+        }
+
+        /// <summary>
+        /// constructor sobrecargado
+        /// </summary>
+        /// <param name="primernumero"></param>
+        public Numero(double primernumero)
+        {
+            this.numero = primernumero;
+        }
+
+        /// <summary>
+        /// Validacion del string para saber si es un double
+        /// </summary>
+        /// <param name="NumeroString"></param>
+        /// <returns></returns>
+        private double validarNumero(string NumeroString)
+        {
+            double primernumero = 0;
+            bool resultado = double.TryParse(NumeroString, out primernumero);
+            if (resultado == false)
+            {
+                primernumero = 0;
+            }
+            else
+            {
+                primernumero = 1;
+            }
+            return primernumero;
+        }
+
+        /// <summary>
+        /// setter del string parseado a double
+        /// </summary>
+        /// <param name="primernumero"></param>
+        private void setNumero(string primernumero)
+        {
+            if (validarNumero(primernumero) == 1)
+            {
+                double.TryParse(primernumero, out numero);
+            }
+            else
+            {
+                MessageBox.Show("Uno de los numeros es erroneo");
+            }
+        }
+
+        /// <summary>
+        /// getter de un numero del tipo "Numero"
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public double getNumero(Numero numero)
+        {
+            double retorno;
+            retorno = this.numero;
+
+            return retorno;
+        }
+
+    }
+}
