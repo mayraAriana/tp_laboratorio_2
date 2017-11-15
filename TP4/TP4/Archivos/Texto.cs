e5c1a56d874f9a0f01d8fd56d90db4641aa753b6
mayraAriana <alvarezmayra.ariana@gmail.com>
2017-11-15 10:19:23 -0300

    TP4
    Navegador

---
+++ TP4/TP4/Archivos/Texto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public string archivo;
+        /// <summary>
+        /// Constructor del archivo de texto
+        /// </summary>
+        /// <param name="archivo"></param>
+        public Texto(string archivo)
+        {
+            this.archivo = archivo;   
+        }
+        /// <summary>
+        /// Guarda los datos que se le pasan por parametro en un archivo de texto
+        /// </summary>
+        /// <param name="datos">datos que se van a guardar en el archivo de texto</param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(this.archivo, true))
+                {
+                    writer.WriteLine(datos);
+                }
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Lee los datos desde el archivo de texto y los guarda en la lista
+        /// </summary>
+        /// <param name="datos">Lista donde se almacenaran los datos leidos</param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            string cadena;
+            try
+            {
+                using (StreamReader reader = new StreamReader(this.archivo))
+                {
+                    while (null != (cadena = reader.ReadLine()))
+                    {
+                        datos.Add(cadena);
+                    }
+                }
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TP4/TP4/Hilo/Descargador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        // Delegados
+        public delegate void Porciento(int porciento);
+        public delegate void FinDeLaDescarga(string html);
+
+        //Eventos
+        public event Porciento Porcentaje;
+        public event FinDeLaDescarga DescargaFinalizada;
+
+
+        private string html;
+        private Uri direccion;
+
+
+        /// <summary>
+        /// Constructor del descargador
+        /// </summary>
+        /// <param name="direccion"></param>
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.Porcentaje(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            this.DescargaFinalizada(this.html);
+        }
+    }
+}
